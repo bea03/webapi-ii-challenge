a616cd3407efda2010d4542cd68b24b00eb36240
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-01 16:25:49 -0600

    starting project

---
+++ index.js
@@ -0,0 +1,24 @@
+//imports
+const express = require('express');
+const postRouter = require('./routers/post-Router.js');
+
+//create a server
+const server = express();
+
+//teach express how to read json from body this is middleware
+server.use(express.json());
+
+//telling the server to use the router for /api/posts
+server.use('/api/posts', postRouter);
+
+//request handlers
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Building RESTful APIs with Express</h>
+      <p>Welcome to the Lambda Building RESTful APIs with Express</p>
+    `);
+  });
+
+  server.listen(5000, () => {
+    console.log('\n*** Server Running on http://localhost:5000 ***\n');
+  })
